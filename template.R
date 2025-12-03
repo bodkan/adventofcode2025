9916c70af2003ed39d1f8d11a863c55113978e58
Martin Petr <git@bodkan.net>
2025-12-03 12:17:54 +0100

    Update template script

--- template.R
+++ template.R
@@ -11,12 +11,18 @@ DAY <- NA
 
 example_result1 <- NA
 
+# sanity check for later refactorings
+# stopifnot(example_result1 == )
+
 cat("Part 1, example data:", example_result1, "\n")
 
 ########################################
 # full data run
 
 full_result1 <- NA
+
+# sanity check for later refactorings
+# stopifnot(full_result1 == )
 
 cat("Part 1, full data:", full_result1, "\n")
 
@@ -31,11 +37,15 @@ cat("-------------\n")
 
 example_result2 <- NA
 
+# stopifnot(example_result2 == )
+
 cat("Part 2, example data:", example_result2, "\n")
 
 ########################################
 # full data run
 
 full_result2 <- NA
+
+# stopifnot(full_result2 == )
 
 cat("Part 2, full data:", full_result2, "\n")
