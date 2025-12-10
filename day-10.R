8af6d23e22c1b1cead355b6842206de1ea29de79
Martin Petr <git@bodkan.net>
2025-12-10 18:46:56 +0100

    Add day 10, part 1 solution

---
+++ day-10.R
@@ -0,0 +1,111 @@
+source("utils.R")
+
+DAY <- 10
+
+read_machines <- function(kind) {
+  machines <-
+    readLines(get_path(DAY, kind)) |>
+    lapply(\(x) strsplit(x, " ")[[1]]) |>
+    lapply(\(x) {
+      final <- strsplit(gsub("\\[|\\]", "", x[1]), "")[[1]] == "#"
+      init <- rep(FALSE, length(final))
+      buttons <-
+        strsplit(gsub("\\(|\\)", "", grep("\\(", x, value = TRUE)), ",") |>
+        lapply(\(i) { buttons <- init; buttons[as.integer(i) + 1] <- TRUE; buttons } )
+      list(init = init, final = final, buttons = buttons)
+    })
+  machines
+}
+
+########################################
+# Part 1
+########################################
+
+push_buttons <- function(lights, buttons) {
+  xor(lights, buttons)
+}
+
+key <- function(lights) paste(as.integer(lights), collapse = "")
+
+search <- function(machine) {
+  init <- machine$init
+  final <- machine$final
+  buttons <- machine$buttons
+
+  # initialize queue with the starting lights configuration of the current machine
+  queue <- list(list(n = 0, lights = init))
+  # tracker of visited configurations
+  visited <- list()
+
+  repeat {
+    # extract the first item of the queue
+    item <- queue[[1]]; queue[[1]] <- NULL
+    n <- item$n
+    lights <- item$lights
+
+    for (b in buttons) {
+      # create a new lights configuration
+      next_lights <- push_buttons(lights, b)
+
+      # return the count of presses needed to reach the final configuration
+      if (all(next_lights == final)) {
+        return(n = n + 1)
+      }
+
+      # if the new configuration of lights hasn't been explored yet, add it
+      # to the queue for further exploration
+      if (!key(next_lights) %in% visited) {
+        visited <- append(visited, key(next_lights))
+        queue <- append(queue, list(list(n = n + 1, lights = next_lights)))
+      }
+    }
+  }
+}
+
+########################################
+# example data test
+
+example_machines <- read_machines("example")
+example_result1 <- sum(sapply(example_machines, search))
+
+stopifnot(example_result1 == 7)
+
+cat("Part 1, example data:", example_result1, "\n")
+
+########################################
+# full data run
+
+full_machines <- read_machines("full")
+
+full_result1 <- sum(sapply(full_machines, search))
+
+# sanity check for later refactorings
+stopifnot(full_result1 == 432)
+
+cat("Part 1, full data:", full_result1, "\n")
+
+cat("-------------\n")
+
+########################################
+# Part 2
+########################################
+
+########################################
+# example data test
+
+example_result2 <- NA
+
+# stopifnot(example_result2 == )
+
+cat("Part 2, example data:", example_result2, "\n")
+
+########################################
+# full data run
+
+full_result2 <- NA
+
+# stopifnot(full_result2 == )
+
+cat("Part 2, full data:", full_result2, "\n")
+
+cat("-------------\n")
